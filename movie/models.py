4050c2ce66c754ebf7f66bfab1e79fbdedd99bab
Your Name <you@example.com>
2022-11-21 20:04:38 -0700

    Created Movie model, serializer, and POST function

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    director = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    release_date = models.DateField()
+    likes = models.IntegerField(default=0)
--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import Http404
+from serializers import MovieSerializer
+from rest_framework.decorators import APIView
+from rest_framework import Response
+from rest_framework import status
 
 # Create your views here.
+
+class MovieList(APIView):
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
+        serializer.is_valid()
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
